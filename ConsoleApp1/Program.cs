ae0ee140f608245caa7527f67dfec8326f4f7c0e
pc1 <cute_js@daum.net>
2023-12-19 17:19:24 +0900

    overoading

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,44 +4,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Win32;
 
 namespace ConsoleApp1
 {
-    class myLib
+    static class myLib
     {
-        public string GetToken(string str, int n, char ch)  //str: " 1 2 3 " "1, 2, 3", (n)번째, ',' ' '
+        public static string GetToken(string str, int n, char ch)  //str: " 1 2 3 " "1, 2, 3", (n)번째, ',' ' '
         {
             return str.Split(ch)[n];
         }
     }
+
+    class Point
+    {
+        int x, y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double Dist1(Point p1, Point p2)
+        {
+            
+                return Math.Sqrt(Math.Pow((p2.x - p1.x), 2) + Math.Pow((p2.y - p1.y), 2));
+            
+        }
+        public static double operator - (Point p1, Point p2)
+        {
+            int d1 = p1.x - p2.x;
+            int d2 = p1.y - p2.y;
+            return Math.Sqrt(d1 * d1 + d2 * d2);
+        }
+        public static int operator *(Point p1, Point p2)
+        {
+            int d1 = p1.x - p2.x;
+            int d2 = p1.y - p2.y;
+            return d1*d2;
+        }
+    }
+    
+
     internal class Program
     {
-        static void Main(string[] args)
+        public void Func1()
         {
-            myLib my = new myLib();
-            
-                object obj;
-                int i = 10, j = 20;
-                double d = 1.5, e = 3.1;
-            obj = i;
-                WriteLine("Hello world({0},{1}) \n Main function({1},{0})", obj, j);
-            obj = d;
-            while (true)
-            {
-                try
-                {
-                    
-                   
-                    string str = ReadLine();
-                    i = int.Parse(my.GetToken(str,0,' ');
-                    j = int.Parse(str.Split(' ')[1]);
+            Point p1 = new Point(10, 20);
+            Point p2 = new Point(30, 40);
+            double distance = p2.Dist1(p2, p1);
+            WriteLine("두 점 p1, p2의 거리는 {0}",distance);
+            WriteLine("두 점 p1, p2의 거리는 {0}", p2-p1);
+            WriteLine("두 점 p1, p2의 사각형의 넓이는 {0}", p2 * p1);
 
-                    WriteLine($"HELLO WORLD ({i}, {j})\nMain function({obj}, {e})");
-                }
-                catch (Exception e1) { WriteLine(e1.Message);
-                    WriteLine("좋은 말로 할때 입력을 잘 하시오!!");
-                }
-            }
+        }
+        public static void Main(string[] args)
+        {
+            Program a = new Program();
+            a.Func1();
+            
         }
     }
 }
